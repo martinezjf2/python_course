4ed0a7fe2a17dceece864a6d431c68d3117d5209
Jeffrey Martinez <martinezjf2@gmail.com>
2022-01-21 20:31:27 -0500

    first commit

---
+++ assignment_4.py
@@ -0,0 +1,31 @@
+
+
+# Create a Tuple initialized with 15 to 25 related values such as automobile types, states, names, etc. Complete the following steps using the Tuple you have just created. Steps:
+
+# Initialize with values.
+
+
+
+
+# Display the contents in a single statement.
+
+
+
+
+# Iterate through the collection displaying the output as a single complete sentence for each value.
+
+
+
+
+# Use the f-string format to control the output.
+
+
+
+
+# Repeat the output in reverse order using a different context string.
+
+
+
+
+
+# Test your program until it works and the output looks nice.
